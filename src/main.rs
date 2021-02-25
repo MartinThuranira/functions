b9976c71cbd1293a7cb6e210b26fd0844fcfdf58
Martin Thuranira <38188355+MartinThuranira@users.noreply.github.com>
2021-02-25 12:03:12 +0300

    added function called five

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 fn main() {
+    let x = five();
+    println!("The value of x is: {}", x);
     println!("Hello, world!");
     another_function();
+    five();
 }
 fn another_function() {
     println!("Another function.");
+}
+fn five() -> i32 {
+    5
 }
